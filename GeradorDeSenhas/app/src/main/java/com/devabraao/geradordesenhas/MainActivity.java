0474b275bdcf6d8831da09dfd35d0af3ad52ccc9
Carlos Abraao <71728180+CarlosAbraao@users.noreply.github.com>
2021-06-10 09:42:36 -0300

    Criado gerador de senhas

---
+++ GeradorDeSenhas/app/src/main/java/com/devabraao/geradordesenhas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.devabraao.geradordesenhas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
